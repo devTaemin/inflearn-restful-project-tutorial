b394cbbee701b861c1a66fe10d683acfa7c6870b
Taemin Im <devTaemin@gmail.com>
2023-04-27 17:20:15 +0900

    update validation api

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,9 +1,13 @@
 package com.example.restfulwebservice.user;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -11,6 +15,11 @@ import java.util.Date;
 @NoArgsConstructor
 public class User {
     private Integer id;
+
+    @Size(min = 2, message = "Name은 2글자 이상 입력해주세요")
+    @NotBlank
     private String name;
+
+    @Past
     private Date joinDate;
 }
