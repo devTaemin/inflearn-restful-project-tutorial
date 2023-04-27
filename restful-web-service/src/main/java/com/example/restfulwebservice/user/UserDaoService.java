94c9aed003e517ce189a4c738694b670ea36ed7b
Taemin Im <devTaemin@gmail.com>
2023-04-27 13:33:40 +0900

    update get user

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -0,0 +1,46 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+    private static int usersCount = 0;
+    static {
+        users.add(new User(1, "taemin", new Date()));
+        usersCount++;
+
+        users.add(new User(2, "jimin", new Date()));
+        usersCount++;
+
+        users.add(new User(3, "joomin", new Date()));
+        usersCount++;
+    }
+
+    public List<User> findAll() {
+        return this.users;
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++usersCount);
+        }
+
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+}
