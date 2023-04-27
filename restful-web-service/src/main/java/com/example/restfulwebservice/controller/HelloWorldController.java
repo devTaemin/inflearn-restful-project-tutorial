43a4c8d4dbaa1a125b4488f9015cb6007818c545
Taemin Im <devTaemin@gmail.com>
2023-04-27 11:19:42 +0900

    Update HelloWorldController.java

--- restful-web-service/src/main/java/com/example/restfulwebservice/controller/HelloWorldController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.restfulwebservice.controller;
 
+import com.example.restfulwebservice.dto.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,5 +18,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }
