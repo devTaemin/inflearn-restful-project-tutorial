bfad49d2e0fa0cef5f4bc4297d739d6d73c55b29
Taemin Im <devTaemin@gmail.com>
2023-04-28 12:04:46 +0900

    update hateoas

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,12 +1,21 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -23,14 +32,34 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable(value = "id") int id) {
+    public Resource<User> retrieveUser(@PathVariable(value = "id") int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
 
-        return user;
+        // HATEOAS
+        Resource<User> resource = new Resource<>(user);
+
+//        ControllerLinkBuilder linkTo = ControllerLinkBuilder.linkTo(
+//                ControllerLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+
+        // Filter
+//        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+//                .filterOutAllExcept("id", "name", "joinDate", "password", "ssn", "_links");
+//
+//        FilterProvider filters = new SimpleFilterProvider()
+//                .addFilter("UserInfo", filter);
+//
+//        MappingJacksonValue mapping = new MappingJacksonValue(user);
+//        mapping.setFilters(filters);
+//
+//        return mapping;
+
+        return resource;
     }
 
     @PostMapping("/users")
